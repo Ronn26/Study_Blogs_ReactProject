2df98ab1499db098b0c17269b2b9c2ff738ba81d
Ronn26 <rchachire4@gmail.com>
2024-09-25 01:14:31 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='fixed top-0 inset-x-0 w-full text-center border-b-2 border-gray-300 shadow-md bg-white py-1 mb-5'>
-        <header className='w-full'>
+        <header className='w-full bg-gray-300'>
         <h1 className='text-3xl font-bold uppercase text-center'>Blogs</h1>
         </header>
 
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -8,11 +8,11 @@ const Pagination = () => {
   if(!totalPages) return null;
   
   return (
-    <div className="fixed bottom-0 bg-white py-1 border-t-2 border-gray-300 w-full inset-x-0 shadow-md">
+    <div className="fixed bottom-0 bg-white py-1 border-t-2 border-gray-300 w-full inset-x-0 shadow-md bg-gray-300">
       <div className="flex gap-x-3 items-center w-11/12 max-w-2xl mx-auto">
         {page > 1 && (
           <button
-            className="rounded-md border-gray-300 border-2 px-4 py-1"
+            className="rounded-md border-gray-300 border-2 px-4 py-1 bg-blue-400"
             onClick={() => handlePageChange(page - 1)}
           >
             Previous
@@ -21,13 +21,13 @@ const Pagination = () => {
 
         {page < totalPages && (
           <button
-            className="rounded-md border-gray-300 border-2 px-4 py-1"
+            className="rounded-md border-gray-300 border-2 px-4 py-1 bg-blue-400"
             onClick={() => handlePageChange(page + 1)}
           >
             Next
           </button>
         )}
-        <p className="text-sm font-semibold ml-auto">
+        <p className="text-sm font-semibold ml-auto bg-orange-200">
           Page {page} of {totalPages}
         </p>
       </div>
